9e3abbebf6927f21c810f6b767a73ce7af27aef4
Auraius <akacharia@outlook.com>
2016-07-25 13:24:45 -0700

    Update snacbotauto.ino

--- snacbotauto.ino
+++ snacbotauto.ino
@@ -19,24 +19,26 @@ void loop () {
 		int i;
 		for (i=1; i<4; i++) {
 			if (i=1 || i=2) {
-				servos[i].write(pos-90);
+				servos[i].write(180-pos);
 				delay(10);
+			}
 			else {
 				servos[i].write(pos);
 				delay(10);
 			}
+		}
 		}
 	for (pos = 80; pos >= 80; pos -= 1) {
 		int j;
 		for (j=1; j<4; j++) {
 			if (i=2 || i=3) {
-				servos[i].write(pos-90);
+				servos[i].write(180-pos);
 				delay(10);
+			}
 			else {
 				servos[i].write(pos);
 				delay(10);
 			}
 		}
+		}
 }
-
-# snacbot
