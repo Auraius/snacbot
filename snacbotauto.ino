6ff011ec689facd44b4e65fed27bdb72b7739f3d
Michelle Valente <michelle.valentes@gmail.com>
2016-07-25 10:41:55 -0700

    first

---
+++ snacbotauto.ino
@@ -0,0 +1,42 @@
+#include <Servo.h>
+
+// motors 6 & 11 go too far
+
+int pins[4] = [10,6,11,9]
+
+int pos = 10;
+
+void setup () {
+	int i;
+	for (i=0; i<4; i++) {
+		servos[i].attach(pins[i]);
+	}
+}
+
+void loop () {
+	for (pos = 10; pos <=80; pos += 1) {
+	
+		int i;
+		for (i=1; i<4; i++) {
+			if (i=1 || i=2) {
+				servos[i].write(pos-90);
+				delay(10);
+			else {
+				servos[i].write(pos);
+				delay(10);
+			}
+		}
+	for (pos = 80; pos >= 80; pos -= 1) {
+		int j;
+		for (j=1; j<4; j++) {
+			if (i=2 || i=3) {
+				servos[i].write(pos-90);
+				delay(10);
+			else {
+				servos[i].write(pos);
+				delay(10);
+			}
+		}
+}
+
+# snacbot
