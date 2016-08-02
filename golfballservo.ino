5127b31c8a00f893a3183d6de178c2af6f851404
Auraius <akacharia@outlook.com>
2016-08-02 12:25:59 -0700

    Finished (single purpose)
    If you publish the message '1' the Golf Ball Dispenser will give you a golf ball.

--- golfballservo.ino
+++ golfballservo.ino
@@ -1,58 +1,47 @@
-/* 
-to control: 1. roscore 2.rostopic pub golfBall std_msgs/UInt16  <on/On or off/Off>
-*/ 
+//to control: 1. roscore 2.rosrun rosserial_python serial_node.py /dev/ttyACM0 3.rostopic pub golfBall std_msgs/UInt16
+ 
 #include <ros.h>
+
 #if defined(ARDUINO) && ARDUINO >= 100
   #include "Arduino.h"
 #else
-  #include <WProgram.h>
+
 #endif
 
 #include <Servo.h> 
-//
 #include <std_msgs/UInt16.h>
 
 ros::NodeHandle  nh;
 
 Servo myservo;
-
 int motorPin = 9;
-int pos = 0;
+int pos = 170;
 
-void run_golf(const std_msgs::UInt32& cmd_msg){
+void run_golf(const std_msgs::UInt16& cmd_msg){
 	
-	int i = cmd_msg.data
-	
-	if (i == on || On) {
-		for (pos = 0; pos <= 180; pos += 1) { 
-			myservo.write(pos);              
-	    		delay(10);                       
-	  	}
-		for (pos = 180; pos >= 0; pos -= 1) { 
-			myservo.write(pos);              
-			delay(10);             
-	 	}
-
-		break;		 
-	}
-	
-	if (i == off || Off) {
-		myservo.write(90);
-		break; 	
-	}
-	
+ int a = cmd_msg.data;
+ int pos =180;
+ if (a== 1) {
+  for (pos = 180; pos >= 0; pos -= 1) { 
+    myservo.write(pos);              
+    delay(10);        
+  }
+  for (pos = 0; pos <= 180; pos += 1) { 
+    myservo.write(pos);              
+    delay(10);        
+  }
+ }
 }
 
 ros::Subscriber<std_msgs::UInt16> sub("golfBall", run_golf);
 
 void setup(){
-	nh.initNode();
-	nh.subscribe(sub);
-	myservo.attach(motorPin); 
+  nh.initNode();
+  nh.subscribe(sub);
+  myservo.attach(motorPin); 
 }
 
-void loop(){
-	nh.spinOnce();
-	delay(1);
+void loop() {
+  nh.spinOnce();
+  delay(1);
 }
-
